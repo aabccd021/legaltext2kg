79b7701e9e0fc23e19be8e7044b5043df7db1613
aabccd021 <aabccd021@gmail.com>
2021-01-07 14:04:02 +0700

    added pasal 5 text query

--- src/main.py
+++ src/main.py
@@ -33,7 +33,7 @@ def process(filename: str) -> None:
         format='turtle',
     )
 
-    # query
+    # query triple and write to file
     query_dir = "sample_queries"
     for q_file in os.listdir(query_dir):
         if q_file.endswith(".sparql"):
@@ -43,7 +43,7 @@ def process(filename: str) -> None:
             # write file
             with open(f'{query_dir}/{q_file}') as f:
                 qres = g.query('\n'.join(f.readlines()))
-            qres = "\n\n".join(['{}\n{}\n{}'.format(*x) for x in qres])
+            qres = '\n\n'.join(['\n'.join([f'{z}' for z in x]) for x in qres])
             q_filename = os.path.splitext(os.path.basename(q_file))[0]
             with open(f'{dir_path}/{q_filename}.txt', 'w') as f:
                 f.write(qres)
--- src/text2dataclass/extract_pasals.py
+++ src/text2dataclass/extract_pasals.py
@@ -7,10 +7,11 @@ from text2dataclass.types import Pasal
 
 
 def extract_pasals(lines: Iterable[str]) -> Iterable[Pasal]:
-    text = "\n".join(lines)
     pasal_strs = extract_to_increment_key_list(lines, get_pasal_key_int)
+    # text = "\n".join(lines)
+    texts = ["\n".join(x) for x in pasal_strs]
     juduls = [get_pasal_key_int(x[0]) for x in pasal_strs]
     juduls = [x for x in juduls if x is not None]
     ayats = [extract_pasal_content(x[1:]) for x in pasal_strs]
     assert len(juduls) == len(ayats)
-    return [Pasal(_key=judul, isi=ayat, text=text) for judul, ayat in zip(juduls, ayats)]
+    return [Pasal(_key=judul, isi=ayat, text=text) for judul, ayat, text in zip(juduls, ayats, texts)]
